41d5868b60eefea1a168e66e53979bd35a2cfd65
o.lazaro3445 <o.lazaro24@info.uas.edu.mx>
2026-06-09 23:06:34 -0700

    Configuracion render

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ sequelize.sync().then(() => {
 
     console.log('Base de datos conectada');
 
-    app.listen(3000, () => {
+   const PORT = process.env.PORT || 3000;
 
-        console.log('Servidor corriendo en puerto 3000');
-
-    });
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
 });
