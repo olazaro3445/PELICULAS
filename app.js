5a939ed1f73fb825cf4b5aa4a1f292a39db4c41b
o.lazaro3445 <o.lazaro24@info.uas.edu.mx>
2026-06-09 23:32:23 -0700

    Migracion PostgreSQL

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
@@ -8,29 +10,20 @@ const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
-
-// Middleware
-
 app.use(cors());
 app.use(express.json());
 
-
-// RUTAS
-
 app.use('/auth', authRoutes);
 app.use('/peliculas', peliculasRoutes);
-
-
-// Base de datos
 
 sequelize.sync().then(() => {
 
     console.log('Base de datos conectada');
 
-   const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en puerto ${PORT}`);
+    });
 
 });
--- database/db.js
+++ database/db.js
@@ -1,8 +1,18 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite'
-});
+const sequelize = new Sequelize(
+    process.env.DATABASE_URL,
+    {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
+);
 
 module.exports = sequelize;
